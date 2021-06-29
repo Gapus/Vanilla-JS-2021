17b2064c919345e2e30a174e339b6b091fbb83ba
Gapus <tjdwnd751@users.noreply.github.com>
2021-06-29 09:54:46 +0900

    Variables-var-const-let

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert("hi");
+const a = 10;
+const b = 2;
+let myName = "Gapus"
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("Hello "+myName);
+
+myName="kim"
+console.log("Hello "+myName);
