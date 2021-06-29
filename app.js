c855a9d991b35b3aa058e3ee99a02c9d682c5aaa
Gapus <tjdwnd751@users.noreply.github.com>
2021-06-29 10:22:33 +0900

    Objects

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const daysOfWeek = ["mon","tue","wed","thu","fri","sat"];
-
-// Gat Item
-console.log(daysOfWeek);
-
-// Add Item
-daysOfWeek.push("sun");
-
-console.log(daysOfWeek);
-
-console.log(daysOfWeek[2]);
+const player = {
+    name:"kim",
+    points:10,
+    fat:true,
+};
+console.log(player);
+//Undate
+player.fat = false;
+console.log(player);
+//Push
+player.lastName="sungjung";
+console.log(player);
