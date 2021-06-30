9af254e6f6e409dbbd47039c4d2b3191050dc9a9
Gapus <tjdwnd751@users.noreply.github.com>
2021-06-30 10:42:02 +0900

    calculator

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const calculator={
     add:function(a,b){
-        console.log(a+b);
+        alert(a+b);
+    },
+    minus:function(a,b){
+        alert(a-b);
+    },
+    times:function(a,b){
+        alert(a*b);
     }
 };
 
 calculator.add(5,6);
+calculator.minus(5,6);
+calculator.times(5,6);
