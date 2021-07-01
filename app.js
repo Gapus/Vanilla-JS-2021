48c0a549c557007477558281fb8dd868117daea1
Gapus <tjdwnd751@users.noreply.github.com>
2021-07-01 13:39:13 +0900

    id=title change

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-const age = parseInt(prompt("How old are you?"));
 
+const title =  document.getElementById("title");
 
+title.innerText="Got you!!"
 
-if(isNaN(age)){
-    console.log("dadsdsad");
-}else if(age<18){
-    console.log("young");
-}else if(age==100){
-    console.log("old");
-}
-else{
-    console.log("ccccccc");
-}
+console.log(title.className);
