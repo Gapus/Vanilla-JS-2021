2b05b82337ec9c91f0639587f4f9071b7120406c
Gapus <tjdwnd751@users.noreply.github.com>
2021-07-05 13:20:33 +0900

    Login Event Create

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+// const loginForm = document.getElementById("login-form");
+// const loginInput=loginForm.querySelector("input");
+// const loginButton=loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput= document.querySelector("#login-form input");
+const greeting= document.querySelector("#greeting");
 
-function handleTitleClick(){
+const HIDDEN_CLAASSNAME = "hidden";
+
+function onLoginSubmit(evnet){
+    evnet.preventDefault();
+    loginForm.classList.add(HIDDEN_CLAASSNAME);
+
+    const username = loginInput.value;
+
+    //"Kim " + username == `kim ${username}`
+
+    localStorage.setItem("username",username);
     
-    h1.classList.toggle("active");
-    
+    greeting.innerText = `kim ${username}`;
+    greeting.classList.remove(HIDDEN_CLAASSNAME)
 }
-// title.onclick=handleTitleClick;
-h1.addEventListener("click",handleTitleClick);
+
+
+
+loginForm.addEventListener("submit",onLoginSubmit);
