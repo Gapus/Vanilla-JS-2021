ec86dc3590caa9c604a5752715420a9ec87b15ea
Gapus <tjdwnd751@users.noreply.github.com>
2021-07-11 20:49:02 +0900

    EndCoding

--- js/background.js
+++ js/background.js
@@ -11,4 +11,8 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `image/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+//document.body.appendChild(bgImage);
+
+document.body.style.backgroundImage = `url(image/${chosenImage})`;
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundRepeat= "no-repeat";
